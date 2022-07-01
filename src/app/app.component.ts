135f46b9b7941c35b1361c26723e63f0cfdf7430
Herman Razafindrakoto <rth.razafy@gmail.com>
2022-07-01 13:25:08 +0300

    First upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import {Component,OnInit} from '@angular/core';
+import {NodeService} from './nodeservice';
+import {TreeNode} from 'primeng/api';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent { 
+
+  title = 'primetree';
+    files1: TreeNode[];
+    
+    files2: TreeNode[];
+    
+    constructor(private nodeService: NodeService) { }
+
+    ngOnInit() {
+        this.nodeService.getFiles().then(files => this.files1 = files);
+        this.nodeService.getFiles().then(files => this.files2 = files);
+    }
+    
+    expandAll(){
+        this.files2.forEach( node => {
+            this.expandRecursive(node, true);
+        } );
+    }
+
+    collapseAll(){
+        this.files2.forEach( node => {
+            this.expandRecursive(node, false);
+        } );
+    }
+    
+    private expandRecursive(node:TreeNode, isExpand:boolean){
+        node.expanded = isExpand;
+        if (node.children){
+            node.children.forEach( childNode => {
+                this.expandRecursive(childNode, isExpand);
+            } );
+        }
+    }
+}
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {RouterModule} from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { NodeService } from './nodeservice';
+
+// import {TreeModule,TreeNode} from 'primeng/tree';
+import {TreeModule} from 'primeng/tree';
+import {ButtonModule} from 'primeng/button';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    TreeModule,
+    ButtonModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path:'',component: AppComponent}
+
+		])
+  ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [NodeService]
+})
+export class AppModule { 
+
+}
